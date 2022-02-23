49239c61e77dc1d2690e320e526ce2d838f7d616
Mix-Liten <mixliten@gmail.com>
2022-02-23 11:20:28 +0800

    update retry when catch error

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const cron = require("node-cron");
-const browserLauncher = require("./utils/browserLauncher.js");
+const mainAPP = require("./main");
 const systemDiffTimeout = require("./utils/systemDiffTimeout");
 const formatDate = require("./utils/formatDate");
 const { checkSchedule } = require("./utils/googleSheet");
@@ -14,77 +14,8 @@ const config = {
   breakDay: ["日", "土"],
 };
 
-const run = async (isOn) => {
-  if (isOn) {
-    console.log(`-------${formatDate("day")}-------`);
-  }
-  try {
-    const browser = await browserLauncher({ headless: true });
-    const page = (await browser.pages())[0];
-    await page.goto(config.url);
-
-    const loginDom = {
-      userid: "#edtUserID_I",
-      password: "#edtPassword_I",
-      submitBtn: "#btnLogin_CD",
-    };
-
-    const checkerDom = {
-      listFrame: 'iframe[name="ErpFrameMenu"]',
-      checker: "#treeView_N0_0_0_7",
-      mainFrame: "iframe#FrameMain",
-      onlineBtn: "#btnOnSwipe_CD",
-      offlineBtn: "#btnOffSwipe_CD",
-    };
-
-    // console.log("open page...");
-
-    await page.type(loginDom.userid, config.userid);
-    await page.type(loginDom.password, config.password);
-    await page.click(loginDom.submitBtn);
-    await page.waitForNavigation();
-
-    // console.log("navigation...");
-
-    const listFrameEleHandle = await page.waitForSelector(checkerDom.listFrame);
-    const listFrameEle = await listFrameEleHandle.contentFrame();
-    await listFrameEle.click(checkerDom.checker);
-
-    // console.log("to checker...");
-
-    await page.waitForTimeout(2000);
-
-    const mainFrameEleHandle = await page.waitForSelector(checkerDom.mainFrame);
-    const mainFrameEle = await mainFrameEleHandle.contentFrame();
-    if (isOn) {
-      // console.log("will log on!");
-      await mainFrameEle.click(checkerDom.onlineBtn);
-      console.log(`↳ ${formatDate("time")} 上班打卡結束!`);
-    } else {
-      // console.log("will log off!");
-      await mainFrameEle.click(checkerDom.offlineBtn);
-      console.log(`↳ ${formatDate("time")} 下班打卡結束!`);
-    }
-
-    // console.log("finish check...");
-    await page.waitForTimeout(2000);
-
-    await browser.close();
-  } catch (error) {
-    console.log("error", error);
-  }
-};
-
 // 排程格式參考
 // https://www.npmjs.com/package/cron#cron-ranges
-
-// cron.schedule("45 07 * * 1-5", () => {
-//   run(true);
-// });
-
-// cron.schedule("05 17 * * 1-5", () => {
-//   run(false);
-// });
 
 const setCronHourAndMinute = (hour, minute) => {
   const [oriHour, oriMinute] = [
@@ -104,7 +35,7 @@ cron.schedule(
     const [isInSchedule, shouldWork] = await checkSchedule(today);
     const isBreakDay = config.breakDay.includes(formatDate("star"));
     if ((isInSchedule && !shouldWork) || (!isInSchedule && isBreakDay)) return;
-    systemDiffTimeout(() => run(true));
+    systemDiffTimeout(() => mainAPP(true, config));
   }
 );
 
@@ -115,7 +46,7 @@ cron.schedule(
     const [isInSchedule, shouldWork] = await checkSchedule(today);
     const isBreakDay = config.breakDay.includes(formatDate("star"));
     if ((isInSchedule && !shouldWork) || (!isInSchedule && isBreakDay)) return;
-    systemDiffTimeout(() => run(false));
+    systemDiffTimeout(() => mainAPP(false, config));
   }
 );
 
---
+++ main.js
@@ -0,0 +1,61 @@
+const browserLauncher = require("./utils/browserLauncher.js");
+const formatDate = require("./utils/formatDate");
+
+const main = async (isOn, config, options) => {
+  if (isOn && !options?.isRetry) {
+    console.log(`-------${formatDate("day")}-------`);
+  }
+  let browser;
+  try {
+    browser = await browserLauncher({ headless: true });
+    const page = (await browser.pages())[0];
+    await page.goto(config.url);
+
+    const loginDom = {
+      userid: "#edtUserID_I",
+      password: "#edtPassword_I",
+      submitBtn: "#btnLogin_CD",
+    };
+
+    const checkerDom = {
+      listFrame: 'iframe[name="ErpFrameMenu"]',
+      checker: "#treeView_N0_0_0_7",
+      mainFrame: "iframe#FrameMain",
+      onlineBtn: "#btnOnSwipe_CD",
+      offlineBtn: "#btnOffSwipe_CD",
+    };
+
+    await page.type(loginDom.userid, config.userid);
+    await page.type(loginDom.password, config.password);
+    await page.click(loginDom.submitBtn);
+    await page.waitForNavigation();
+
+    const listFrameEleHandle = await page.waitForSelector(checkerDom.listFrame);
+    const listFrameEle = await listFrameEleHandle.contentFrame();
+    await listFrameEle.click(checkerDom.checker);
+
+    await page.waitForTimeout(2000);
+
+    const mainFrameEleHandle = await page.waitForSelector(checkerDom.mainFrame);
+    const mainFrameEle = await mainFrameEleHandle.contentFrame();
+    if (isOn) {
+      await mainFrameEle.click(checkerDom.onlineBtn);
+      console.log(`↳ ${formatDate("time")} 上班打卡結束!`);
+    } else {
+      await mainFrameEle.click(checkerDom.offlineBtn);
+      console.log(`↳ ${formatDate("time")} 下班打卡結束!`);
+    }
+
+    await page.waitForTimeout(2000);
+
+    await browser.close();
+  } catch (error) {
+    console.log("error", error);
+    await browser.close();
+    setTimeout(() => {
+      main(isOn, config, { isRetry: true });
+    }, 1000);
+  }
+};
+
+module.exports = main;
